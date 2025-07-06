4eb4d10d9ef661b68dad2b8a8f9f5bd34930334b
renzolas <144304815+renzolas@users.noreply.github.com>
2025-07-05 22:56:22 -0400

    Update can.py

--- can.py
+++ can.py
@@ -10,9 +10,13 @@ st.header("Iniciar sesión")
 username = st.text_input("Usuario")
 password = st.text_input("Contraseña", type="password")
 
+# Usuario y contraseña predeterminados
+default_username = "admin"
+default_password = "admin"
+
 # Validación básica de login
 if st.button("Iniciar sesión"):
-    if username == "admin" and password == "admin":
+    if username == default_username and password == default_password:
         st.success("¡Bienvenido Admin!")
     elif username != "" and password != "":
         st.success("¡Bienvenido Usuario!")
@@ -20,7 +24,7 @@ if st.button("Iniciar sesión"):
         st.error("Usuario o Contraseña Incorrectos.")
 
 # Mostrar canchas disponibles (sólo si está logueado)
-if username == "admin" or username == "user":
+if username == default_username or username != "":
     st.header("Canchas Disponibles")
     courts = [
         {"name": "Cancha 1", "type": "Fútbol", "available_hours": "09:00, 10:00, 11:00"},
